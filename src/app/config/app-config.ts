b514d5dd8dabaa4b7afd53b5cbe2b66706ef0cb6
Luciano Machado <lucianoo_machado@hotmail.com>
2025-11-27 16:27:52 -0300

    fix: app-config url

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -2,6 +2,6 @@ export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1';
 export const ALL_POST_URL = `${API_URL}/api/posts?populate=*`;
 
 export const SITE_NAME = 'Luciano Machado';
-export const SITE_URL = '';
+export const SITE_URL = 'http://3.133.89.152';
 export const SITE_URL_LOCAL = 'http://localhost:3000';
 export const PAGE_SIZE = 8;
