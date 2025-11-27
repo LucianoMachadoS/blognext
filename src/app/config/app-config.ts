8ef2fafa67bbabc3fd13169f308b321324a01c1c
Luciano Machado <lucianoo_machado@hotmail.com>
2025-11-27 14:44:03 -0300

    teste webhook

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -5,4 +5,4 @@ export const ALL_POST_URL = `${API_URL}/api/posts?populate=*`;
 export const SITE_NAME = 'Luciano Machado';
 export const SITE_URL = '';
 export const SITE_URL_LOCAL = 'http://localhost:3000';
-export const PAGE_SIZE = 10;
+export const PAGE_SIZE = 8;
